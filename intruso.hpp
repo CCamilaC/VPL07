158f38e33d8e8c6e2285d1115845b1f5a657ef84
Natan_HCR <nhcr7@hotmail.com>
2022-10-28 14:35:04 -0300

    implementação map

--- intruso.hpp
+++ intruso.hpp
@@ -1,7 +1,11 @@
 #include <string>
+#include <map>
+#include <vector>
 
 class Intruso{
     /*Continue a implementação da classe Intruso*/
+    private:
+        std::map<char,std::vector<int>> _letras;
 
     public:
         void set_senha_vazada(std::string vazou);
