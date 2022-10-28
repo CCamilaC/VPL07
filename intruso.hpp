89331b497cf817e71cf447b6522cb5393db0b9d5
Natan_HCR <nhcr7@hotmail.com>
2022-10-28 16:18:40 -0300

    Criando atributos da classe

--- intruso.hpp
+++ intruso.hpp
@@ -1,3 +1,4 @@
+#include <iostream> 
 #include <string>
 #include <map>
 #include <vector>
@@ -6,6 +7,14 @@ class Intruso{
     /*Continue a implementação da classe Intruso*/
     private:
         std::map<char,std::vector<int>> _letras;
+
+        std::string letraA;
+        std::string letraB;
+        std::string letraC;
+        std::string letraD;
+        std::string letraE;
+        std::string possi_senha;
+        int contador=0;
 
     public:
         void set_senha_vazada(std::string vazou);
